3fb0079c36fe1ecb76003be29ab2058a58d8b0a2
jasonwillyan <jasonwillyan@hotmail.com>
2021-10-29 04:50:33 -0300

    first commit

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+import Chance from 'chance';
+let chance = new Chance();
+
+context('Cadastro', () => {
+    it('Cadastro de novo usuario', () => {
+
+        cy.visit('index.php');
+
+        cy.get                        ('a[class=login]').click();
+        cy.get       ('input#email_create').type(chance.email());
+        cy.get                   ('button#SubmitCreate').click();
+        cy.get                      ('input#id_gender1').check();
+        cy.get ('input#customer_firstname').type(chance.first());
+        cy.get   ('input#customer_lastname').type(chance.last());
+        cy.get                    ('input#passwd').type('12345');
+        cy.get                      ('select#days').select('10');
+        cy.get                    ('select#months').select('10');
+        cy.get                   ('select#years').select('1999');
+        cy.get('input[type=checkbox]').should('have.value', '1');
+        cy.get                           ('input#optin').check();
+        cy.get         ('input#address1').type(chance.address());
+        cy.get                ('input#city').type(chance.city());
+
+        //⬇⬇⬇⬇ Seleciona o elemento na posição x(3) independente do value ou text
+        cy.get('select#id_state > option')
+            .eq(3)
+            .then(element => cy.get('select#id_state').select(element.val()));
+
+        cy.get(      'input#postcode').type(chance.zip());
+        //cy.get       ('select#id_country').select('21');             ///> Quebra o teste  
+        cy.get('input#phone_mobile').type(chance.phone());
+        cy.get     ('input#alias').type(chance.address());
+        cy.get           ('button#submitAccount').click();
+
+        //⬇⬇⬇⬇ Validando a url usando baseUrl + rota
+        cy.url().should('eq', Cypress.config().baseUrl + '/index.php?controller=my-account');
+
+        //⬇⬇⬇⬇ Validando texto (Welcome to your account)
+        cy.get('p[class^=info]').should('contain.text', 'Welcome to your account');
+        
+    });
+});
